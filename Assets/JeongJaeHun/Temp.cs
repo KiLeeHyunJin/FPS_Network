2212e00a667d2a1fab84196ef2d6ec9548d3a8c4
ghdwp98 <ghdwp98@naver.com>
2024-05-08 15:11:20 +0900

    item : 아이템 준비

---
+++ Assets/JeongJaeHun/Temp.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    // 총 발사 로직 짤 때 지연보상 필요. 
+    // 상점 ui 작성 무기 종류 구현 
+    // 스크립터블 오브젝트 
+    //공격력 방어력 강화 가능 
+    // 총 종류 + 칼 + 수류탄 + 연막 정도? 
+    // 
+    // 번호로 무기 스왑 하기 
+
+
+    void Start()
+    {
+         
+    }
+
+    void Update()
+    {
+        
+    }
+}
