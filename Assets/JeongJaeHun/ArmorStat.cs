8e3c200d0a1d044b32ccd119385f2da86d77ab8e
ghdwp98 <ghdwp98@naver.com>
2024-05-09 14:17:34 +0900

    item : 방어구 구현 고민 중...

--- Assets/JeongJaeHun/ArmorStat.cs
+++ Assets/JeongJaeHun/ArmorStat.cs
@@ -1,23 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArmorStat : MonoBehaviour
 {
     // 여기서 플레이어의 아머 스탯을 분류해주고 
     // 아머는 소 중 대 3가지 종류고 그 3가지 종류를 해서 
-    // 플레이어의 
+
+   
 
 
 
-    
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
     }
 }
