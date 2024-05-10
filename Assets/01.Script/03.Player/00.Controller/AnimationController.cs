84fdaad0abb01b4629d3137a834a05ed0dda5733
KIAcademyLHJ <kistudentmeta5@gmail.com>
2024-05-10 17:35:49 +0900

    [EDIT] CharacterController

--- Assets/01.Script/03.Player/00.Controller/AnimationController.cs
+++ Assets/01.Script/03.Player/00.Controller/AnimationController.cs
@@ -3,10 +3,239 @@ using System.Collections;
 using UnityEngine;
 
 
-public class AnimationController : MonoBehaviourPun, IPunObservable
+public class AnimationController : MonoBehaviourPun//, IPunObservable
 {
+    Animator anim;
+    public Animator Anim { get { return anim; } }
+    [SerializeField] float dampingValue;
+
+    int JumpEnterId;
+    int ChangeEnterId;
+
+    //byte[] Pos;
+    //byte layer;
+    Vector3 motionValue;
+
+    short[] changedBitPos;
+    int[] layerId;
+    int[] floatId;
+    Coroutine[] dampingCo;
+    //void SetState(AnimatorState type, bool state)
+    //{
+    //    int idx = (int)type;
+    //    if (state)
+    //        layer |= (byte)(1 << idx);
+    //    else
+    //        layer &= (byte)~(1 << idx);
+    //    anim.SetBool(layerId[(int)type], state);
+    //}
+    public Vector2 MoveValue
+    {
+        set
+        {
+            //SetValue(AnimatorFloatValue.X, value.x);
+            //SetValue(AnimatorFloatValue.Z, value.y);
+            this.ReStartCoroutine(DampingAnimationRoutine(value.x, AnimatorFloatValue.X), ref dampingCo[(int)AnimatorFloatValue.X]);
+            this.ReStartCoroutine(DampingAnimationRoutine(value.y, AnimatorFloatValue.Z), ref dampingCo[(int)AnimatorFloatValue.Z]);
+        }
+    }
+    public float VelocityY
+    {
+        set
+        {
+            //SetValue(AnimatorFloatValue.Y, value);
+            this.ReStartCoroutine(DampingAnimationRoutine(value, AnimatorFloatValue.Y), ref dampingCo[(int)AnimatorFloatValue.Y]);
+        }
+    }
+    //void SaveValue(AnimatorFloatValue type, float _value)
+    //{
+    //    byte answard;
+    //    byte lockValue = AnimatorFloatValue.Y == type ? (byte)10 : (byte)100;
+    //    if (_value < 0)
+    //    {
+    //        answard = byte.MaxValue;
+    //        answard += _value < -1 ? (byte)-lockValue : (byte)(_value * lockValue);
+    //    }
+    //    else
+    //    {
+    //        answard = _value > 1 ? lockValue : (byte)(_value * 100);
+    //    }
+    //    Pos[(int)type] = answard;
+    //}
+    //void SetValue(AnimatorFloatValue type, float _value)
+    //{
+    //    this.ReStartCoroutine(DampingAnimationRoutine(_value, floatId[(int)type], type), ref dampingCo[(int)type]);
+    //    SaveValue(type, _value);
+    //}
+    public void Crouch(bool state)
+    {
+        SetState(AnimatorState.Crouch, state);
+        photonView.RPC("CrouchRPC", RpcTarget.AllViaServer);
+    }
+
+    public void JumpStart()
+    {
+        SetState(AnimatorState.JumpFinish, false);
+        photonView.RPC("JumpRPC", RpcTarget.AllViaServer);
+    }
+
+    public void Die()
+    {
+    }
+    public void MoveRun()
+    {
+        SetState(AnimatorState.Move, true);
+        SetState(AnimatorState.Run, true);
+    }
+    public void MoveWalk()
+    {
+        SetState(AnimatorState.Move, true);
+        SetState(AnimatorState.Run, false);
+    }
+    public void MoveStop()
+    {
+        SetState(AnimatorState.Move, false);
+        SetState(AnimatorState.Run, false);
+    }
+    public void JumpFinish()
+    {
+        SetState(AnimatorState.JumpFinish, true);
+    }
+    void SetState(AnimatorState type, bool state)
+    {
+        anim.SetBool(layerId[(int)type], state);
+    }
+    private void Awake()
+    {
+        SetAnimID();
+        anim = GetComponent<Animator>();
+        anim.applyRootMotion = false;
+        dampingValue = dampingValue <= 0 ? 0.15f : dampingValue;
+        dampingValue = 1 / dampingValue;
+    }
+    void SetAnimID()
+    {
+        JumpEnterId = Animator.StringToHash("JumpEnter");
+        ChangeEnterId = Animator.StringToHash("ChangeEnter");
+
+        int JumpFinishId = Animator.StringToHash("JumpFinish");
+        int ForwardId = Animator.StringToHash("Forward");
+        int JumpId = Animator.StringToHash("Jump");
+        int CrouchId = Animator.StringToHash("Crouch");
+        int MoveId = Animator.StringToHash("Move");
+        int RunId = Animator.StringToHash("Run");
+        int TurnId = Animator.StringToHash("Turn");
+
+        floatId = new int[] { TurnId, JumpId, ForwardId };
+        layerId = new int[] { MoveId, RunId, CrouchId, JumpFinishId };
+        dampingCo = new Coroutine[(int)AnimatorFloatValue.END];
+    }
+    //private void Start()
+    //{
+    //    Pos = new byte[(int)AnimatorFloatValue.END];
+    //    changedBitPos = new short[9];
+    //    layer = 0;
+    //}
+
+    //void UpdateState(byte _inputValue)
+    //{
+    //    for (int i = 0; i < Comparison(_inputValue); i++)
+    //        anim.SetBool(layerId[changedBitPos[i]], !GetState(changedBitPos[i]));
+    //    layer = _inputValue;
+    //}
+
+    //int Comparison(byte _inputValue)
+    //{
+    //    byte answard = (byte)(layer ^ _inputValue);
+    //    int count = 0;
+    //    for (short i = 0; i < 8; i++)
+    //    {
+    //        if ((answard & (1 << i)) != 0)
+    //        {
+    //            changedBitPos[count] = i;
+    //            count++;
+    //        }
+    //    }
+    //    return count;
+    //}
+    //bool GetState(short idx)
+    //{
+    //    byte sour = layer;
+    //    sour &= (byte)(1 << idx);
+    //    return sour != 0;
+    //}
+
+    //void UpdatePos(AnimatorFloatValue type, byte value)
+    //{
+    //    Pos[(int)type] = value;
+    //    float hashValue = GetValue(type);
+    //    anim.SetFloat(floatId[(int)type], hashValue);
+    //}
+    //float GetValue(AnimatorFloatValue type)
+    //{
+    //    float value = Pos[(int)type];
+    //    if(value > 154)
+    //        value = ((byte.MaxValue - value) * -1);
+    //    if (type == AnimatorFloatValue.Y)
+    //        return value * 0.1f;
+    //    return value * 0.01f;
+    //}
+
+    [PunRPC]
+    void CrouchRPC()
+    {
+        anim.SetTrigger(ChangeEnterId);
+    }
+    [PunRPC]
+    void JumpRPC()
+    {
+        anim.SetTrigger(JumpEnterId);
+    }
+    //public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //{
+    //    if (stream.IsWriting)
+    //    {
+    //        stream.SendNext(Pos[(int)AnimatorFloatValue.X]);
+    //        stream.SendNext(Pos[(int)AnimatorFloatValue.Y]);
+    //        stream.SendNext(Pos[(int)AnimatorFloatValue.Z]);
+    //        stream.SendNext(layer);
+    //    }
+    //    else
+    //    {
+    //        UpdatePos(AnimatorFloatValue.X, (byte)stream.ReceiveNext());
+    //        UpdatePos(AnimatorFloatValue.Y, (byte)stream.ReceiveNext());
+    //        UpdatePos(AnimatorFloatValue.Z, (byte)stream.ReceiveNext());
+    //        UpdateState((byte)stream.ReceiveNext());
+    //    }
+    //}
+
+    IEnumerator DampingAnimationRoutine(float value, AnimatorFloatValue type)
+    {
+        float startValue = anim.GetFloat((int)type);
+        float time = 0;
+        float currentValue;
+        while (time <= 1)
+        {
+            time += Time.deltaTime * dampingValue;
+            currentValue = Mathf.Lerp(startValue, value, time);
+            anim.SetFloat(floatId[(int)type], currentValue);
+            //SaveValue(type, currentValue);
+            yield return null;
+        }
+    }
+    public enum AnimatorState
+    {
+        Move,
+        Run,
+        Crouch,
+        JumpFinish,
+        END
+    }
+
+    public enum AnimatorFloatValue
+    { X, Y, Z, END }
     public enum MoveType
-    { 
+    {
         Walk,
         Run,
         Stop,
@@ -15,249 +244,8 @@ public class AnimationController : MonoBehaviourPun, IPunObservable
         Stand,
 
         Jump,
-        JumpMove,
         JumpFinish,
         JumpSlide,
         END
-    }
-    public enum ValueType
-    {
-        Leg,
-        Velocity,
-        END
-    }
-
-    Animator anim;
-    public Animator Anim { get { return anim; } }
-    Controller controller;
-    [SerializeField] float dampingValue;
-
-    float capsuleRadius;
-
-    bool isRun;
-    bool isMove;
-    int JumpFinishId;
-    int JumpEnterId;
-    int ForwardId;
-    int JumpId;
-    int CrouchId;
-    int ChangeEnterId;
-    int MoveId;
-    int RunId;
-    int TurnId;
-    int JumpDirId;
-
-    private Vector2 MoveValue { get; set; }
-    private float JumpLeg { get; set ; }
-    private float VelocityY { get; set; }
-    public void SetMoveValue(Vector2 value)
-    {
-        MoveValue = value;
-        anim.SetFloat(TurnId, MoveValue.x);
-        anim.SetFloat(ForwardId, MoveValue.y);
-    }
-    public void SetJumpLeg(float value)
-    {
-        JumpLeg = value;
-        anim.SetFloat(JumpDirId, JumpLeg);
-    }
-    public void SetVelocityY(float value)
-    {
-        VelocityY = value;
-        anim.SetFloat(JumpId, VelocityY);
-    }
-
-    private void Awake()
-    {
-        SetAnimID();
-        anim = GetComponent<Animator>();
-        anim.applyRootMotion = false;
-        controller = GetComponent<Controller>();
-        dampingValue = dampingValue <= 0 ? 0.15f : dampingValue;
-    }
-    void SetAnimID()
-    {
-        JumpFinishId = Animator.StringToHash("JumpFinish");
-        JumpEnterId = Animator.StringToHash("JumpEnter");
-        ForwardId = Animator.StringToHash("Forward");
-        JumpId = Animator.StringToHash("Jump");
-        CrouchId = Animator.StringToHash("Crouch");
-        ChangeEnterId = Animator.StringToHash("ChangeEnter");
-        MoveId = Animator.StringToHash("Move");
-        RunId = Animator.StringToHash("Run");
-        TurnId = Animator.StringToHash("Turn");
-        JumpDirId = Animator.StringToHash("JumpDir");
-    }
-    private void Start()
-    {
-        capsuleRadius = controller.Capsule.radius;
-    }
-    public void JumpStart()
-    {
-        anim.SetBool(JumpFinishId, false);
-        photonView.RPC("JumpStartRPC", RpcTarget.AllViaServer);
-    }
-    public void JumpMove()
-    {
-        photonView.RPC("JumpMoveRPC", RpcTarget.AllViaServer);
-    }
-    public void JumpFinish()
-    {
-        anim.SetBool(JumpFinishId, true);
-        photonView.RPC("JumpFinishRPC", RpcTarget.AllViaServer);
-    }
-    public void Crouch()
-    {
-        anim.SetBool(CrouchId, false);
-        photonView.RPC("CrouchRPC", RpcTarget.AllViaServer, false);
-    }
-    public void Stand()
-    {
-        anim.SetBool(CrouchId, true);
-        photonView.RPC("CrouchRPC", RpcTarget.AllViaServer, true);
-    }
-    public void MoveWalk()
-    {
-        anim.SetBool(MoveId, true);
-        anim.SetBool(RunId, false);
-        photonView.RPC("MoveWalkRPC", RpcTarget.AllViaServer);
-    }
-    public void MoveRun()
-    {
-        anim.SetBool(MoveId, false);
-        anim.SetBool(RunId, true);
-        photonView.RPC("MoveRunRPC", RpcTarget.AllViaServer);
-    }
-    public void MoveStop()
-    {
-        anim.SetBool(MoveId, false);
-        anim.SetBool(RunId, false);
-        photonView.RPC("MoveStopRPC", RpcTarget.AllViaServer);
-    }
-    public void Die()
-    {
-         photonView.RPC("DieRPC", RpcTarget.AllViaServer);
-    }
-
-    [PunRPC]
-    void JumpStartRPC()
-    {
-        //anim.applyRootMotion = false;
-        //
-        anim.SetTrigger(JumpEnterId);
-        controller.ScaleCapsule(true);
-        controller.Capsule.radius = 0.05f;
-    }
-
-    [PunRPC]
-    void JumpMoveRPC()
-    {
-        anim.SetFloat(ForwardId, JumpLeg);
-        anim.SetFloat(JumpId, VelocityY);
-    }
-
-    [PunRPC]
-    void JumpFinishRPC()
-    {
-        //anim.applyRootMotion = true;
-        //
-        controller.ScaleCapsule(false);
-        controller.Capsule.radius = capsuleRadius;
-    }
-
-    [PunRPC]
-    void CrouchRPC(bool state)
-    {
-        anim.SetTrigger(ChangeEnterId);
-        controller.ScaleCapsule(state);
-    }
-
-    [PunRPC]
-    void MoveWalkRPC()
-    {
-        //if (isMove == false)
-        //{
-        //    isMove = true;
-        //    anim.SetBool(MoveId, true);
-        //}
-        //if (isRun)
-        //{
-        //    isRun = false;
-        //    anim.SetBool(RunId, false);
-        //}
-        //StartCoroutine(DampingAnimation(ForwardId, MoveValue.y, dampingValue));
-        //StartCoroutine(DampingAnimation(TurnId, MoveValue.x, dampingValue));
-
-        Debug.Log($"Forward {MoveValue.y}");
-        Debug.Log($"Turn {MoveValue.x}");
-    }
-
-    [PunRPC]
-    void MoveRunRPC()
-    {
-        //if (isMove == false)
-        //{
-        //    isMove = true;
-        //    anim.SetBool(MoveId, true);
-        //}
-        //if (isRun == false)
-        //{
-        //    isRun = true;
-        //    anim.SetBool(RunId, true);
-        //}
-        //StartCoroutine(DampingAnimation(ForwardId, MoveValue.y, dampingValue));
-       //StartCoroutine(DampingAnimation(TurnId, MoveValue.x, dampingValue));
-
-    }
-
-    [PunRPC]
-    void MoveStopRPC()
-    {
-        if (isMove)
-        {
-            isMove = false;
-            //anim.SetBool(MoveId, false);
-        }
-        if (isRun)
-        {
-            isRun = false;
-            //anim.SetBool(RunId, false);
-        }
-        //StartCoroutine(DampingAnimation(ForwardId,0, dampingValue * 0.5f));
-        //StartCoroutine(DampingAnimation(TurnId, 0, dampingValue * 0.5f));
-    }
-
-    [PunRPC]
-    void DieRPC()
-    {
-
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(MoveValue);
-            stream.SendNext(JumpLeg);
-            stream.SendNext(VelocityY);
-
-        }
-        else
-        {
-            MoveValue = (Vector2)stream.ReceiveNext();
-            JumpLeg = (float)stream.ReceiveNext();
-            VelocityY = (float)stream.ReceiveNext();
-        }
-
-    }
-    IEnumerator DampingAnimation(int _id, float _value,float _dampingValue)
-    {
-        float time = 0;
-        while(1 >= time)
-        {
-            time += Time.deltaTime;
-            anim.SetFloat(_id, _value, _dampingValue, Time.deltaTime);
-            yield return null;
-        }
     }
 }
