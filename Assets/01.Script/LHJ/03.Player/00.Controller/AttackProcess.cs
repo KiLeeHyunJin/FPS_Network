ed4970cd57987fd6b67a479f48a57ea7db80501e
KIAcademyLHJ <kistudentmeta5@gmail.com>
2024-05-23 15:24:03 +0900

    [FEAT] Fix Hit Flow

--- Assets/01.Script/LHJ/03.Player/00.Controller/AttackProcess.cs
+++ Assets/01.Script/LHJ/03.Player/00.Controller/AttackProcess.cs
@@ -1,56 +1,37 @@
+using Photon.Pun;
 using System;
 using System.Collections;
 using UnityEngine;
 
-public class AttackProcess
+public class AttackProcess : MonoBehaviourPun
 {
-     // 다른 곳에서 생성자를 참조 하고 있어서.. 확인하고 주석처리 하기. 
 
-    float sleepTime;
-
-    Transform aim;
-    Controller owner;
-    Coroutine recoilCo;
-    public AttackProcess(Controller _owner)
+    public void Fire(AnimationController.AnimatorWeapon _weaponType)
     {
-        owner = _owner;
-    }
-    public void SetAimTransform(Transform _aim)
-    => aim = _aim;
-
-    void Recoil()
-        => owner.StartCoroutined(RecoilRoutine(), ref recoilCo);
-
-
-    public Vector3 Attack()
-    {
-        if (sleepTime != 0)
-            sleepTime = 0;
-
-        if (aim == null)
-            return Vector3.zero;
-
-        //Recoil();
-
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hitInfo, Mathf.Infinity))
+        switch (_weaponType)
         {
-            if (hitInfo.collider != null)
-            {
-                return (hitInfo.transform.position - aim.position).normalized;
-
-                //if (Physics.Raycast(aim.position, direction, out hitInfo, Mathf.Infinity))
-                //    return hitInfo.collider.GetComponent<Controller>();
-            }
-        }
-        return Vector3.zero;
-    }
-
-    public IEnumerator RecoilRoutine()
-    {
-        while (true)
-        {
-            sleepTime += Time.deltaTime;
-            yield return null;
+            case AnimationController.AnimatorWeapon.Pistol:
+                GunAttack(); 
+                break;
+            case AnimationController.AnimatorWeapon.Rifle:
+                GunAttack();
+                break;
+            case AnimationController.AnimatorWeapon.Sword:
+                SwordAttack();
+                break;
         }
     }
+
+    void GunAttack()
+    {
+
+    }
+
+    void SwordAttack()
+    { 
+    
+    }
+
+
+
 }
--- Assets/01.Script/LHJ/03.Player/00.Controller/Controller.cs
+++ Assets/01.Script/LHJ/03.Player/00.Controller/Controller.cs
@@ -402,26 +402,18 @@ public class Controller : MonoBehaviourPun, IPunObservable
             inputController.InputActive = false;
         }
     }
-
-    public void Damage(int _damage, int _actorNumber) // 총알의 주인 ActorNumber 
+    [PunRPC]
+    void CallDamage(int _damage,int _actorNumber)
     {
-        if (PhotonNetwork.IsMasterClient == false)
-        {
-            Debug.Log("Is Not Master");
-            return;
-        }
-        Debug.Log($"Damage {_damage} ");
-
         hp -= equipController.ShieldCheck(_damage);
-        Debug.Log($"equipShield HP : {hp}");
-        
+        Debug.Log("CallDam");
         if (HpBar != null)
         {
             HpBar.value = Percent(hp, maxHp);
         }
 
         if (hp <= 0)
-        {          
+        {
             //if (Mine) //PhotonView.IsMine 쓰는거 맞나?? 잘 모르겠네... 
             {
                 Player deathPlayer = photonView.Owner;
@@ -431,11 +423,10 @@ public class Controller : MonoBehaviourPun, IPunObservable
                     , deathPlayer.NickName, lastShooterPlayer.NickName);
                 photonView.RPC("LogMessage", RpcTarget.AllBufferedViaServer, msg);
 
-                deathPlayer.SetProperty(DefinePropertyKey.DEATH, deathPlayer.GetProperty<int>(DefinePropertyKey.DEATH)+1);
-                lastShooterPlayer.SetProperty(DefinePropertyKey.KILL, lastShooterPlayer.GetProperty<int>(DefinePropertyKey.KILL)+1);
+                deathPlayer.SetProperty(DefinePropertyKey.DEATH, deathPlayer.GetProperty<int>(DefinePropertyKey.DEATH) + 1);
+                lastShooterPlayer.SetProperty(DefinePropertyKey.KILL, lastShooterPlayer.GetProperty<int>(DefinePropertyKey.KILL) + 1);
 
             }
-
             sensor.StopRoutine();
             animController.Die();
             Cursor.lockState = CursorLockMode.None;
@@ -443,6 +434,17 @@ public class Controller : MonoBehaviourPun, IPunObservable
             cameraController.CameraPriority = 0;
             inputController.InputActive = false;
         }
+    }
+
+    public void Damage(int _damage, int _actorNumber) // 총알의 주인 ActorNumber 
+    {
+        if (PhotonNetwork.IsMasterClient == false)
+        {
+            return;
+        }
+        Debug.Log("Damage");
+        photonView.RPC("CallDamage", photonView.Owner, _damage, _actorNumber);
+
     }
 
     // slider bar 조정 용 
