eead75e41ebab0d269726e5712323db1f42f60ff
이형필 <hexagongame@naver.com>
2024-05-23 14:13:01 +0900

    feat : KillLogPanel 구현중

---
+++ Assets/HP/Scripts/KillLogPanel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillLogPanel : MonoBehaviour
+{
+   public KillLogEntry killInfoEntry;
+    public Transform entryPos;
+
+
+
+}
