44e10c8c752fa6bc3dcb534b40e400a349e67ffd
KIAcademyLHJ <kistudentmeta5@gmail.com>
2024-05-24 14:48:52 +0900

    REMOVE Shop Item ID

--- Assets/JeongJaeHun/Script/PurchasePrefab.cs
+++ Assets/JeongJaeHun/Script/PurchasePrefab.cs
@@ -69,7 +69,7 @@ public class PurchasePrefab : MonoBehaviour
             inventory.AddItem(item);
             if (item.itemPrefab != null)
             {
-                Debug.Log($"{item.itemPrefab.name},{Id}"); //물건의 아이템 형과 id가 제대로 들어오는지 확인해보기. 
+               // Debug.Log($"{item.itemPrefab.name},{Id}"); //물건의 아이템 형과 id가 제대로 들어오는지 확인해보기. 
             }
             this.ReStartCoroutine(SucessPurchase(), ref coroutine);//만약 진행중인 코루틴이 있으면 중지시키고 코루틴을 실행해줘야함. 
 
